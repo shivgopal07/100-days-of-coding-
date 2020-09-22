cbf3f760c6447c07289788d813f1b649d22d6778
Shiv Gopal Verma <56026986+shivgopal07@users.noreply.github.com>
2020-09-23 00:45:02 +0530

    creating class
    Display present date and time

---
+++ class.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+class Datee
+{
+	void display()
+	{
+		Date d=new Date();
+		System.out.println("Today's date is:- " +d);
+	}
+}
+class dinak
+{
+	public static void main(String[] args)
+	{
+		Datee obj=new Datee();
+		obj.display();
+	}
+}
